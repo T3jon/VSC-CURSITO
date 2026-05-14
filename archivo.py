dd6b016933aaad029563e6f3b681430b515e256d
Juan Romero <juan.d.moreno0120@outlook.com>
2026-05-14 16:55:36 -0500

    nuevo cambio

---
+++ archivo.py
@@ -0,0 +1,2 @@
+print('hola, mi nombre es JUAN y mi apellidos es MORENO')
+print('espero que tengas un muy PEDORRO dia')
--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-numA = 30
-numB = 20
-
-res = numB - numA
-console.log(res)
+function name(params) {
+    
+}
